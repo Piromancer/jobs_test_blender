25a0d958730856cfdd3dc651f1a7d65877695c59
Kozlov Alexey <36414392+Flame0n@users.noreply.github.com>
2021-04-07 18:56:43 +0300

    Python39 (#223)
    * Changed python version to python3.9

--- scripts/run.sh
+++ scripts/run.sh
@@ -11,6 +11,7 @@ ENGINE=${9:-FULL}
 TOOL=${10:-2.91}
 RETRIES=${11:-2}
 UPDATE_REFS=${12:-No}
-python -m pip install --user -r ../jobs_launcher/install/requirements.txt
+python3.9 -m pip install --user -r ../jobs_launcher/install/requirements.txt
 
-python ../jobs_launcher/executeTests.py --test_filter $TESTS_FILTER --file_filter $FILE_FILTER --tests_root ../jobs --work_root ../Work/Results --work_dir Blender28 --cmd_variables Tool "blender$TOOL" RenderDevice $RENDER_DEVICE ResPath "$CIS_TOOLS/../TestResources/rpr_blender_autotests_assets" PassLimit $ITER rx $RX ry $RY SPU $SPU threshold $THRESHOLD engine $ENGINE retries $RETRIES UpdateRefs $UPDATE_REFS
+python3.9 ../jobs_launcher/executeTests.py --test_filter $TESTS_FILTER --file_filter $FILE_FILTER --tests_root ../jobs --work_root ../Work/Results --work_dir Blender28 --cmd_variables Tool "blender$TOOL" RenderDevice $RENDER_DEVICE ResPath "$CIS_TOOLS/../TestResources/rpr_blender_autotests_assets" PassLimit $ITER rx $RX ry $RY SPU $SPU threshold $THRESHOLD engine $ENGINE retries $RETRIES UpdateRefs $UPDATE_REFS
+
